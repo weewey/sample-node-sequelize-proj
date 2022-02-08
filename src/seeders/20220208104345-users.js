e91972e15a07595f903e455afdf2ed8f7c9f1f88
yewwee <ychua@thoughtworks.com>
2022-02-08 19:03:52 +0800

    add default values to createdAt and updatedAt for users and add seeder

---
+++ src/seeders/20220208104345-users.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.bulkInsert('Users', [
+            {
+                id: 1,
+                name: 'Rubi',
+                age: 31,
+                gender: "F"
+            },
+            {
+                id: 2,
+                name: 'Randy',
+                age: 32,
+                gender: "M"
+            },
+            {
+                id: 3,
+                name: 'Apple',
+                age: 18,
+                gender: "F"
+            },
+            {
+                id: 4,
+                name: 'Mango',
+                age: 14,
+                gender: "F"
+            },
+            {
+                id: 5,
+                name: 'Ferry',
+                age: 37,
+                gender: "M"
+            },
+            {
+                id: 6,
+                name: 'Johnson',
+                age: 55,
+                gender: "M"
+            },
+            {
+                id: 7,
+                name: 'Larry',
+                age: 45,
+                gender: "M"
+            },
+            {
+                id: 8,
+                name: 'Ryne',
+                age: 12,
+                gender: "F"
+            },
+            {
+                id: 9,
+                name: 'Christopher',
+                age: 24,
+                gender: "M"
+            },
+        ], {})
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.bulkDelete('Users', {
+            id: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        });
+    }
+};
