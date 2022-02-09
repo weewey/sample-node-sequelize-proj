0a66fd5bbce0a351a56cd6a93163678d402a191f
yewwee <ychua@thoughtworks.com>
2022-02-09 12:22:29 +0800

    add jest and babel-runtime

--- src/models/index.js
+++ src/models/index.js
@@ -5,14 +5,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    sequelize = new Sequelize(config.database, config);
+    sequelize = new Sequelize(config);
 }
 
 fs
@@ -21,7 +21,7 @@ fs
         return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
     .forEach(file => {
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        const model = require(path.join(__dirname, file)).default(sequelize, Sequelize.DataTypes);
         db[model.name] = model;
     });
 
