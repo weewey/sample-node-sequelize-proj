9b5d50df4f32cd75778388bc7956efc324ed8e75
yewwee <ychua@thoughtworks.com>
2022-02-12 12:24:14 +0800

    add create in user service

--- __tests__/services/user_service.test.js
+++ __tests__/services/user_service.test.js
@@ -8,4 +8,23 @@ describe("UserService", () => {
 
         expect(spy).toBeCalled()
     })
+
+    describe('#CREATE', () => {
+        it("should call UserRepository.create", async () => {
+            const userAttrs = {name: "Test", age: 10, gender: "A"}
+            const spy = jest.spyOn(UserRepository,
+                "create").mockResolvedValue([]);
+
+            await UserService.create(userAttrs)
+            expect(spy).toBeCalled()
+        })
+
+        it('should throw the error from UserRepository', async () => {
+            const userAttrs = {name: "Test", age: 10, gender: "A"}
+            jest.spyOn(UserRepository,
+                "create").mockRejectedValue(new Error("test"));
+            await expect(UserService.create(userAttrs)).rejects.toThrow(new Error("test"))
+        });
+    });
+
 })
--- src/services/user_service.js
+++ src/services/user_service.js
@@ -1,8 +1,16 @@
 import UserRepository from "../repository/user_repository.js";
 
 class UserService {
-    static async getUsers(){
+    static async getUsers() {
         return UserRepository.fetchAll()
+    }
+
+    static async create(userAttrs) {
+        try {
+            return await UserRepository.create(userAttrs)
+        } catch (e) {
+            throw e
+        }
     }
 }
 
